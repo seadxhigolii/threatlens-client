d9d0582790e0ab2b093eeda9d40fbeaa679f0b01
Sead Xhigoli <sead.xhigoli1@gmail.com>
2024-12-21 23:43:54 +0100

    add electron

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            contextIsolation: true,
+            nodeIntegration: false
+        }
+    });
+
+    mainWindow.loadURL('http://localhost:4200');
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -7,6 +8,29 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
-  title = 'threatlens-client';
+  constructor(private http: HttpClient) {}
+
+  onStartScan(): void {
+    this.http.post('https://localhost:7169/api/start-scan', {}).subscribe({
+      next: (response) => {
+        console.log('Scan started successfully:', response);
+      },
+      error: (error) => {
+        console.error('Error starting scan:', error);
+      }
+    });
+  }
+
+  onStopScan(): void {
+    this.http.post('https://localhost:7169/api/stop-scan', {}).subscribe({
+      next: (response) => {
+        console.log('Scan started successfully:', response);
+      },
+      error: (error) => {
+        console.error('Error starting scan:', error);
+      }
+    });
+  }
 }
